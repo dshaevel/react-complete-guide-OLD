ab43a13fd83f0826f60f1fb845d484f0f3d0cf1c
David Shaevel <david@davidshaevel.com>
2018-04-09 00:32:32 -0500

    Validating Props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './Person.css';
 import withClass2 from '../../../hoc/WithClass2';
@@ -38,5 +39,12 @@ class Person extends Component {
     // ]
   }
 }
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
 
 export default withClass2(Person, classes.Person);
