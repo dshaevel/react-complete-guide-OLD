739d7d5b5368937f301bbe3689f6f47712a5f277
David Shaevel <david@davidshaevel.com>
2018-04-08 22:34:11 -0500

    Returning Adjacent Elements and Understanding Higher Order Components (HOC)

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-class App extends Component {
+class App extends PureComponent {
   constructor(props) {
     super(props);
 
@@ -28,10 +28,11 @@ class App extends Component {
     console.log('[App.js] Inside componentDidMount()');
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('[UPDATE App.js] Inside shouldComponentUpdate()', nextProps, nextState);
-    return true;
- }
+//   shouldComponentUpdate(nextProps, nextState) {
+//     console.log('[UPDATE App.js] Inside shouldComponentUpdate()', nextProps, nextState);
+//     return nextState.persons !== this.state.persons ||
+//       nextState.showPersons !== this.state.showPersons;
+//  }
 
  componentWillUpdate(nextProps, nextState) {
     console.log('[UPDATE App.js] Inside componentWillUpdate()', nextProps, nextState);
@@ -85,6 +86,7 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
+        <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
         <Cockpit
           appTitle={this.props.title}
           showPersons={this.state.showPersons}
